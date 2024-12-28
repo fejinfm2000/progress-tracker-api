1fab20203e625922f1a3694806545fb46265b16d
Fejin Francis <fejinfm2000@gmail.com>
2024-12-29 00:19:13 +0530

    CORS Configuration

--- src/main/java/com/fm/progresstracker/entity/User.java
+++ src/main/java/com/fm/progresstracker/entity/User.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
